4b5d85789d8af82d3343fefb99fac4752c427d85
Adrian <adr654231@gmail.com>
2021-09-17 14:07:35 +0200

    add social data

---
+++ src/data/socialData.ts
@@ -0,0 +1,24 @@
+import GitHubIcon from "@material-ui/icons/GitHub";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+
+export const socialData = [
+	{
+		id: 1,
+		title: "GitHub",
+		icon: GitHubIcon,
+		href: "https://github.com/Sharpedov",
+	},
+	{
+		id: 2,
+		title: "Facebook",
+		icon: FacebookIcon,
+		href: "https://www.facebook.com/adrian.piatek.56481",
+	},
+	{
+		id: 3,
+		title: "LinkedIn",
+		icon: LinkedInIcon,
+		href: "https://www.linkedin.com/in/adrian-pi%C4%85tek-942204209/",
+	},
+];
