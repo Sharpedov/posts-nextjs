259510e063e149148438408f9b0b5db68cbd8703
Adrian <adr654231@gmail.com>
2021-09-17 14:08:45 +0200

    add content to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { socialData } from "src/data/socialData";
 import styled from "styled-components";
+import Link from "next/link";
 
 interface IProps {}
 
 const Footer = ({}: IProps) => {
 	return (
 		<FooterContainer>
-			<Wrapper></Wrapper>
+			<Wrapper>
+				<Column1>Made by Adrian Piątek</Column1>
+				<Column2>
+					<SocialContainer>
+						<span>SOCIAL</span>
+						<SocialList>
+							{socialData.map((social, i) => (
+								<Link key={`${social.title}-${i}`} passHref href={social.href}>
+									<a target="_blank" rel="noopener noreferrer">
+										<SocialItem aria-label={social.title}>
+											<social.icon className="footerSocialItem__icon" />
+										</SocialItem>
+									</a>
+								</Link>
+							))}
+						</SocialList>
+					</SocialContainer>
+				</Column2>
+			</Wrapper>
 		</FooterContainer>
 	);
 };
@@ -20,11 +40,64 @@ const FooterContainer = styled.footer`
 
 const Wrapper = styled.div`
 	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 30px 0;
 	max-width: 1050px;
 	width: 100%;
 	margin: 0 auto;
 
+	@media ${({ theme }) => theme.breakpoints.md} {
+		flex-direction: row;
+		gap: 0 30px;
+		justify-content: space-between;
+	}
+
 	@media ${({ theme }) => theme.breakpoints.xl} {
 		max-width: 1180px;
+	}
+`;
+
+const Column1 = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const Column2 = styled(Column1)``;
+
+const SocialContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	> span {
+		margin-bottom: 10px;
+		font-size: 1.4rem;
+	}
+`;
+
+const SocialList = styled.ul`
+	display: flex;
+	gap: 0 10px;
+`;
+
+const SocialItem = styled.li`
+	display: flex;
+	background: rgba(255, 255, 255, 0.3);
+	border-radius: 1px;
+	padding: 0.5rem;
+	cursor: pointer;
+	opacity: 1;
+	transition: opacity 0.25s, transform 0.25s;
+
+	&:hover {
+		opacity: 0.65;
+	}
+	&:active {
+		transform: scale(0.92);
+	}
+
+	.footerSocialItem__icon {
+		font-size: 2.2rem;
 	}
 `;
