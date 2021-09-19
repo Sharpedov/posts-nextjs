97e1390098f3820f512901be847192fd62fca634
Adrian <adr654231@gmail.com>
2021-09-20 00:27:45 +0200

    modify api auth getLoggedUser

--- pages/api/auth/getLoggedUser.ts
+++ pages/api/auth/getLoggedUser.ts
@@ -1,25 +1,9 @@
-import cookie from "cookie";
-import jwt_decode from "jwt-decode";
+import dbConnect from "mongodb/dbConnect";
+import { authMiddleware } from "mongodb/middlewares/authMiddleware";
+import User from "mongodb/models/User";
 
-export default async function handler(req, res) {
-	const cookies = cookie.parse(req.headers.cookie || "");
-
-	if (!cookies.auth) {
-		res.setHeader(
-			"Set-Cookie",
-			cookie.serialize("auth", "", {
-				httpOnly: true,
-				secure: process.env.NODE_ENV !== "development",
-				expires: new Date(0),
-				sameSite: "strict",
-				path: "/",
-			})
-		);
-
-		return res.json({ success: false });
-	}
-
-	let decoded = jwt_decode(cookies.auth);
-
-	res.json({ success: true, data: decoded });
-}
+export default authMiddleware(async function handler(req, res) {
+	await dbConnect();
+	const user = await User.findOne({ _id: req.token.userId });
+	res.json(user);
+});
