987b06a5f8495cb8533da6059e8820f8fe6e0668
Adrian <adr654231@gmail.com>
2021-09-20 18:05:05 +0200

    modify logout api

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,16 +1,7 @@
-import cookie from "cookie";
+import { authMiddleware } from "mongodb/middlewares/authMiddleware";
+import { clearAuthTokens } from "mongodb/utils/authTokenUtils";
 
-export default async function handler(req, res) {
-	res.setHeader(
-		"Set-Cookie",
-		cookie.serialize("auth", "", {
-			httyOnly: true,
-			secure: process.env.NODE_ENV !== "development",
-			expires: new Date(0),
-			sameSite: "strict",
-			path: "/",
-		})
-	);
-
-	res.status(200).json({ success: true });
-}
+export default authMiddleware(async function handler(req, res) {
+	clearAuthTokens(res);
+	res.end();
+});
